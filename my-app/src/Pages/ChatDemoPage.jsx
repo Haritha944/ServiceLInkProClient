20c544ec333ebf3fbb2fb4d69690525068edfa7a
Haritha944 <harithams23@gmail.com>
2024-10-28 14:25:50 +0530

    cahnge into wss

--- my-app/src/Pages/ChatDemoPage.jsx
+++ my-app/src/Pages/ChatDemoPage.jsx
@@ -47,7 +47,7 @@ const ChatDemoPage = () => {
       }
 
       console.log('Before WebSocket initialization.............///');
-      ws.current = new WebSocket(`https://api.trendyfoot.online/ws/chat/${senderId}/${receiverId}/${senderType}/${receiverType}/`);
+      ws.current = new WebSocket(`wss://api.trendyfoot.online/ws/chat/${senderId}/${receiverId}/${senderType}/${receiverType}/`);
 
       ws.current.onopen = () => {
           console.log('WebSocket connection established................./////');
